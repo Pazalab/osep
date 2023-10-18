4abf8ce6cd2502bf4fe0a8a04289aabcc24af85a
justallyt <albertjunk2030@gmail.com>
2023-10-19 00:05:32 +0300

    Updates

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,7 +3,9 @@ import env_analysis from "../assets/env-research.png"
 import conservation from "../assets/forest-conservation.png"
 import global from "../assets/global.png"
 import { NavLink } from "react-router-dom"
-
+import future from "../assets/future.jpg"
+import research from "../assets/research.jpg"
+import together from "../assets/together.jpg"
 const data = [
     {
         id: 0,
@@ -49,16 +51,33 @@ const AboutSection = () => {
                                                           <p>Osep Kenya is a reputable CBO founded on a passion and commitment to create a sustainable future for our planet. We believe that through collective action, we can make a positive impact on the environment and create a more just and equitable world for all.</p>
                                                   </div>
                                                   <div className="about-intro-column">
-                                                          <NavLink to={'/about-us'}>Read More
-                                                                       <div className="tree-bubble">
-                                                                                  <span></span>
-                                                                                  <span></span>
-                                                                                  <span></span>
-                                                                                  <span></span>
-                                                                       </div>
-                                                          </NavLink>
+                                                          <NavLink to={'/about-us'}>Read More</NavLink>
                                                   </div>
                                     </div>
+                         </div>
+
+                         <div className="about-more-description">
+                                     <div className="about-more-moja">
+                                                  <img src={future} alt="" />
+                                                 <div className="about-more-texts">
+                                                          <h4>Securing the Future</h4>
+                                                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem facilis, dolores quidem nam harum sint consequuntur recusandae enim dignissimos ducimus?</p>
+                                                 </div>
+                                     </div>
+                                      <div className="about-more-moja">
+                                                <img src={research} alt="" />
+                                                 <div className="about-more-texts">
+                                                          <h4>Data Collections</h4>
+                                                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem facilis, dolores quidem nam harum sint consequuntur recusandae enim dignissimos ducimus?</p>
+                                                 </div>
+                                     </div>
+                                      <div className="about-more-moja">
+                                                  <img src={together} alt="" />
+                                                 <div className="about-more-texts">
+                                                          <h4>Sensitization & Awareness</h4>
+                                                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem facilis, dolores quidem nam harum sint consequuntur recusandae enim dignissimos ducimus?</p>
+                                                 </div>
+                                     </div>
                          </div>
             </div>
     </div>
