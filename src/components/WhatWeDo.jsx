815cbb67eadebc1ce9986987e7c68dbe22e2ba20
justallyt <albertjunk2030@gmail.com>
2023-10-21 23:04:06 +0300

    Updates

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -1,13 +1,45 @@
-
+import { NavLink } from "react-router-dom"
+import leafy from "../assets/leafy.png"
 const WhatWeDo = () => {
   return (
     <div className="what-we-do">
               <div className="inner-row">
                        <div className="what-we-do-content">
                                    <div className="intro">
-                                             <h3>What We Do</h3>
+                                             <h4>What We Do</h4>
                                              <h1>Projects We Engage in to Achieve Results that Last</h1>
                                              <p>We bring together people from all sides and draw from deep expertise to tackle environmental issues from every angle. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus aspernatur nihil officiis provident tenetur dolore possimus quia odit eveniet ratione.</p>
+                                   </div>
+                                   <div className="leafy-abstract">
+                                             <img src={leafy} alt="" />
+                                   </div>
+
+                                   <div className="what-we-do-row">
+                                               <div className="project-moja">
+                                                            <h3>Environmental Education and Awareness</h3>
+                                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum excepturi corrupti veritatis sed aliquid, nihil est. Sequi delectus iure iste.</p>
+                                                            <NavLink to={'/'}>Explore More</NavLink>
+                                               </div>
+                                               <div className="project-moja">
+                                                            <h3>Forest Conservation & Restoration</h3>
+                                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum excepturi corrupti veritatis sed aliquid, nihil est. Sequi delectus iure iste.</p>
+                                                            <NavLink to={'/'}>Explore More</NavLink>
+                                               </div>
+                                               <div className="project-moja">
+                                                            <h3>Sustainability Practices</h3>
+                                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum excepturi corrupti veritatis sed aliquid, nihil est. Sequi delectus iure iste.</p>
+                                                            <NavLink to={'/'}>Explore More</NavLink>
+                                               </div>
+                                               <div className="project-moja">
+                                                            <h3>Environmental Advocacy & Policy</h3>
+                                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum excepturi corrupti veritatis sed aliquid, nihil est. Sequi delectus iure iste.</p>
+                                                            <NavLink to={'/'}>Explore More</NavLink>
+                                               </div>
+                                               <div className="project-moja">
+                                                            <h3>Research & Environmental Analysis</h3>
+                                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum excepturi corrupti veritatis sed aliquid, nihil est. Sequi delectus iure iste.</p>
+                                                            <NavLink to={'/'}>Explore More</NavLink>
+                                               </div>
                                    </div>
                        </div>
               </div>
